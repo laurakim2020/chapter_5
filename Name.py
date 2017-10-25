4ad7fe74fe3ceb3d7041401f380ede122e392812
laurakim2020 <laurakim2020@gmail.com>
2017-10-25 11:46:05 -0400

    Name Thing

---
+++ Name.py
@@ -0,0 +1,12 @@
+name=input("name")
+if name=="Sachin":
+    print("Hi Sachin Group 1")
+    
+elif name=="Laura":
+    print("Hi Laura Group1")
+    
+elif name=="Liam":
+    print("Hi Liam Group 2")
+else:
+    print(name, "I don't know you")
+    
